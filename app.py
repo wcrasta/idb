980e680711eac2728d89a577c6aa3144c345c909
Zihao <zihaozhu@utexas.edu>
2017-03-17 00:26:22 -0500

    Created links to the html pages

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/about.html', methods=['GET'])
+def about():
+    return render_template('about.html',title = "about")
+
+@app.route('/games.html', methods=['GET'])
+def games():
+    return render_template('games.html', title = "games")
+
+@app.route('/reviews.html', methods = ['GET'])
+def reviews():
+    return render_template('reviews.html', title="reviews")
+
+@app.route('/platforms.html', methods = ['GET'])
+def platforms():
+    return render_template('platforms.html', title='platforms')
+
+@app.route('/studios.html', methods = ['GET'])
+def studios():
+    return render_template('studios.html', title='studios')
+
 # Run the Flask app.
 if __name__ == '__main__':
     app.run()
