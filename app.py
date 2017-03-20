e95b93b1d6d74dc15a5a4e4ebce56c5bdcc516b7
wcrasta <wcrasta@utexas.edu>
2017-03-20 18:04:45 -0500

    Populating grids and fixed time

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
+import time
+
 # Create the Flask application.
 app = Flask(__name__)
 
-#app.debug = True
+app.debug = True
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
@@ -26,7 +29,8 @@ def about():
 
 @app.route('/games.html', methods=['GET'])
 def games():
-    return render_template('games.html', title="games")
+	items = populateGrid()
+	return render_template('games.html', items=items, title="games")
 
 
 @app.route('/reviews.html', methods=['GET'])
@@ -39,9 +43,20 @@ def platforms():
     return render_template('platforms.html', title='platforms')
 
 
-@app.route('/studios.html', methods=['GET'])
+@app.route('/studios.html', methods=['GET'])	
 def studios():
     return render_template('studios.html', title='studios')
+
+def populateGrid():
+	with open('games.json') as json_data:
+		items = json.load(json_data)
+		for x in items:
+			if(x['first_release_date']):
+				x['first_release_date'] = time.strftime("%m-%d-%Y", time.gmtime(x['first_release_date']/1000))
+			print(x['first_release_date'])
+		return items
+
+populateGrid()
 
 # Run the Flask app.
 if __name__ == '__main__':
