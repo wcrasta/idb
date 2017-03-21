e025db47d54193e25f59f414e5f40eb058f6b915
Colin Thornton <colin@cs.utexas.edu>
2017-03-21 18:11:32 -0500

    Updated models.py to contain more attributes

--- models.py
+++ models.py
@@ -7,7 +7,13 @@ from app import db
 #studioToPlatform = db.Table('studioToPlatform', db.Column('studio_id', db.Integer, db.ForeignKey(
 #    'studio.id')), db.Column('platform_id', db.Integer, db.ForeignKey('platform.id')))
 
+game_category = { 0:"Main Game", 1:"DLC/Add on", 2:"Expansion", 3:"Bundle", 4:"Standalone expansion"}
+game_status = {0:"Released", 2:"Alpha", 3:"Beta", 4:"Early Access", 5:"offline", 6:"Cancelled"}
+esrb = {1:"RP", 2:"EC", 3:"E", 4:"E10+", 5:"T", 6:"M", 7:"AO"}
+
 genres={33:"Arcade", 32:"Indie", 31:"Adventure", 30:"Pinball", 26:"Quiz/Trivia", 25:"Hack and slash/Beat 'em up", 24:"Tactical", 16:"Turn-based strategy (TBS)", 15:"Strategy", 14:"Sport", 13:"Simulator", 12:"Role-playing (RPG)",11:"Real Time Strategy (RTS)", 10:"Racing", 9:"Puzzle", 8:"Platform", 7:"Music", 5:"Shooter", 4:"Fighting", 2:"Point-and-click"}
+
+
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,10 +24,21 @@ class Game(db.Model):
 
     genre = db.Column(db.String(80))
 
+    rating = db.Column(db.Numeric(2,2))
+
+    storyline = db.Column(db.Text)
+
+    category = db.Column(db.Integer)
+
+    esrb = db.Column(db.Integer)
+
+    status = db.Column(db.Integer)
 
     studio_id = db.Column(db.Integer, db.ForeignKey('studio.id'))
 
     reviews = db.relationship('Reviews', backref='game')
+
+    video = db.Column(db.String(128))
 
     #image = db.Column(db.LargeBinary)
     image = db.Column(db.String(128))
@@ -32,12 +49,13 @@ class Platform(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     api_id = db.Column(db.Integer)
 
+    created_at = db.Column(db.DateTime)
     name = db.Column(db.String(80), unique=True)
     summary = db.Column(db.Text)
     review = db.relationship('Reviews', backref='platform')
     # discuss relationship of game
     # games =
-
+    generation = db.Column(db.Integer)
     #image = db.Column(db.LargeBinary)
     image = db.Column(db.String(128))
     website = db.Column(db.String(80))
@@ -47,10 +65,17 @@ class Studio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     #logo = db.Column(db.LargeBinary, nullable = True)
+
+    platform = db.Column(db.String(128))
+
     logo = db.Column(db.String(128))
     description = db.Column(db.Text, nullable = True)
     game = db.relationship('Game', backref = 'studio')
 
+    published_Games= db.Column(db.String(128))
+
+    created_at = db.Column(db.DateTime)
+    website = db.Column(db.String(128))
 
 #    platform = db.relationship(
 #        'Platform', secondary=studioToPlatform, backref=db.backref('studio', lazy='dynamic'))
@@ -66,6 +91,7 @@ class Reviews(db.Model):
     created_at = db.Column(db.DateTime)
     views = db.Column(db.Integer)
 
+    video = db.Column(db.String(128))
     introduction = db.Column(db.Text, nullable = True)
     content = db.Column(db.Text)
     conclusion = db.Column(db.Text)
