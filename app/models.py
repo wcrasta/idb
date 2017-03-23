8a6ebf5ad63b34567520a9b6135d5856dd69ad8d
wcrasta <wcrasta@utexas.edu>
2017-03-23 18:39:20 -0500

    Comments and makefile change

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
-from database import db
-#from app import db
+#!/usr/bin/env python3
 
+# pylint: disable = no-member
+# pylint: disable = missing-docstring
+# pylint: disable = invalid-name
+# pylint: disable = too-few-public-methods
+from database import db
+# from app import db
 
 
 class Game(db.Model):
@@ -42,7 +47,9 @@ class Game(db.Model):
     release_date = db.Column(db.DateTime)
     website = db.Column(db.String(80))
 
+
 class Platform(db.Model):
+
     """ Platform is a model that represents the various video game consoles
     that exist  and it contains information regarding the games the console
     supports, the reviews of the console, the generation, image, website and
@@ -61,10 +68,11 @@ class Platform(db.Model):
     generation = db.Column(db.Integer)
     image = db.Column(db.String(128))
     website = db.Column(db.String(80))
-    studio = db.relationship('Studio', backref='platform',lazy="dynamic")
+    studio = db.relationship('Studio', backref='platform', lazy="dynamic")
 
 
 class Studio(db.Model):
+
     """
         Studio is a model that represents the companies that publish and develop
         video games. Studios have informatino regarding the name, the logo, the games
@@ -76,12 +84,14 @@ class Studio(db.Model):
     platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'))
 
     logo = db.Column(db.String(128))
-    description = db.Column(db.Text, nullable = True)
-    game = db.relationship('Game', backref = 'studio', lazy= "dynamic")
+    description = db.Column(db.Text, nullable=True)
+    game = db.relationship('Game', backref='studio', lazy="dynamic")
     created_at = db.Column(db.DateTime)
     website = db.Column(db.String(128))
 
+
 class Reviews(db.Model):
+
     """
         Reviews is a model that represents the consumer reviews of various
         games that involve discussing the console the game is on and the company
@@ -99,9 +109,10 @@ class Reviews(db.Model):
     views = db.Column(db.Integer)
 
     video = db.Column(db.String(128))
-    introduction = db.Column(db.Text, nullable = True)
+    introduction = db.Column(db.Text, nullable=True)
     content = db.Column(db.Text)
     conclusion = db.Column(db.Text)
     positive = db.Column(db.Text)
     negative = db.Column(db.Text)
     url = db.Column(db.String(80))
+    assert(id is not None)
