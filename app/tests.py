381b8a46211e7f6db6436f1d0ed75c97af52312a
Colin Thornton <colin@cs.utexas.edu>
2017-03-23 20:59:33 -0500

    Finished comments in...

--- app/tests.py
+++ app/tests.py
@@ -9,6 +9,7 @@ class TestCase(unittest.TestCase):
 
     # Zihao's unit tests
 
+    # Tests that reviews are being properly stored in games
     def test_game_to_review(self):
         game = Game(name="Zelda", api_id=1)
         reviews = Reviews(title="Review of Zelda")
@@ -16,6 +17,7 @@ class TestCase(unittest.TestCase):
         self.assertTrue(game.reviews != None)
         self.assertTrue(reviews.game != None)
 
+    # Tests that platform reviews are being stored in platforms
     def test_platform_to_review(self):
         platform = Platform()
         platform.name = "Test platform"
@@ -24,6 +26,8 @@ class TestCase(unittest.TestCase):
         self.assertTrue(platform.review != None)
         self.assertTrue(reviews.platform != None)
 
+    # Tests that game can be stored in studio and that the relation
+    # Automatically stores the studio in game
     def test_studio_to_game(self):
         studio = Studio()
         game = Game()
@@ -31,6 +35,8 @@ class TestCase(unittest.TestCase):
         self.assertTrue(game.studio == studio)
         self.assertTrue(studio.game != None)
 
+    # Tests that game can be stored in platform and that the relation
+    # Automatically stores the platform in game
     def test_platform_to_game(self):
         platform = Platform()
         game = Game()
@@ -38,6 +44,8 @@ class TestCase(unittest.TestCase):
         self.assertTrue(game.platform == platform)
         self.assertTrue(platform.games != None)
 
+    # Tests that studio can be stored in platform and that the relation
+    # Automatically stores the platform in studio
     def test_platform_to_studio(self):
         platform = Platform()
         studio = Studio()
@@ -45,6 +53,8 @@ class TestCase(unittest.TestCase):
         self.assertTrue(studio.platform == platform)
         self.assertTrue(platform.studio != None)
 
+    # Tests to see that relationships will properly override when
+    # a variable is changed
     def test_overriding_relationship(self):
         platform = Platform()
         studio = Studio()
@@ -55,31 +65,38 @@ class TestCase(unittest.TestCase):
         self.assertTrue(studio.platform == platform2)
         self.assertTrue(platform != platform2)
 
+    # Checks default values are set to none
     def test_unpopulated_is_none(self):
         game = Game()
         self.assertTrue(game.platform_id == None)
 
+    # Makes sure that the lists are initalized to empty lists
     def test_unpopulated_list_is_not_none(self):
         game = Game()
         self.assertTrue(game.reviews != None)
         self.assertTrue(game.reviews.count() == 0)
 
+    # Makes sure that it is not possible to make a list 
+    # attribute none
     def test_list_can_never_be_none(self):
         game = Game()
         with self.assertRaises(TypeError):
             game.reviews = None
 
+    # Tests that keys can be set by name
     def test_nonkey_attributes_populated(self):
         platform = Platform(generation=4)
         studio = Studio()
         platform.studio.append(studio)
         self.assertTrue(studio.platform.generation == 4)
 
+    # Tests that no relationship exist before establishing
     def test_no_relationship(self):
         studio = Studio(name="Riot")
         game = Game(name="LoL")
         self.assertTrue(game.studio_id == None)
 
+    # Tests that the relationship automatically set up the foreign keys
     def test_foreignkey(self):
         studio = Studio()
         studio.name = "Test Studio"
@@ -87,6 +104,7 @@ class TestCase(unittest.TestCase):
         studio.game.append(game)
         self.assertTrue(game.studio_id == studio.id)
 
+    # Tests that the classes are loading dynamically
     def test_lazy_dynamic(self):
         platform = Platform()
         platform.name = "Wii"
@@ -95,12 +113,14 @@ class TestCase(unittest.TestCase):
         platform.games.append(game)
         self.assertTrue("SELECT" in str(platform.games))
 
+    # Makes sure that the websites do not start with http
     def test_link(self):
         reviews = Reviews()
         reviews.title = "Zelda review"
         reviews.url = "zelda.com"
         self.assertFalse("http" in reviews.url)
 
+    # Tests that multiple relations are set up as attributes are being set
     def test_multiple_relationships(self):
         platform = Platform(name="test")
         game = Game(name="Zelda")
