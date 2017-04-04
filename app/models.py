96087bae56629d415f64ded46f047864ba0e0f8c
Warren Crasta <warrencrasta@gmail.com>
2017-04-03 22:03:04 -0500

    Proofread and re-formatted about half of the technical report.

--- app/models.py
+++ app/models.py
@@ -13,21 +13,17 @@ import os
 import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.debug = True
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-#app.config[
- #   'SQLALCHEMY_DATABASE_URI'] = os.environ['our_secret']
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = os.environ['our_secret']
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-#   os.path.join(basedir, 'app.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['our_secret']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'app.db')
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db.init_app(app)
 db = SQLAlchemy(app)
 
 
@@ -42,30 +38,18 @@ class Game(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
-
     api_id = db.Column(db.Integer)
     summary = db.Column(db.Text)
-
     genre = db.Column(db.String(256))
-
     rating = db.Column(db.Float)
-
     storyline = db.Column(db.Text)
-
     category = db.Column(db.String(256))
-
     esrb = db.Column(db.String(256))
-
     status = db.Column(db.String(256))
-
     platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'))
-
     studio_id = db.Column(db.Integer, db.ForeignKey('studio.id'))
-
     reviews = db.relationship('Reviews', backref='game', lazy="dynamic")
-
     video = db.Column(db.String(256))
-
     image = db.Column(db.String(256))
     release_date = db.Column(db.DateTime)
     website = db.Column(db.String(256))
@@ -127,37 +111,6 @@ class Game(db.Model):
             assert website != ""
             self.website = website
 
-        # assert api_id > 0
-        # assert summary != None
-        # assert genre != None
-        # assert rating > -1
-        # assert storyline != None
-        # assert category > -1
-        # assert category < 50
-        # assert esrb > -1
-        # assert status > -1 and status < 50
-        # assert video != None
-        # assert image != None
-        # assert release_date != None
-        # assert website != None
-        # self.name = name
-        # self.api_id = api_id
-        # self.summary = summary
-        # self.genre = genre
-        # self.rating = rating
-        # self.storyline = storyline
-        # self.category = category
-        # self.esrb = esrb
-        # self.status = status
-        # self.platform_id = platform_id
-        # self.studio_id = studio_id
-        # self.reviews = reviews
-        # self.video = video
-        # self.image = image
-        # self.release_date = release_date
-        # self.website = website
-
-
 class Platform(db.Model):
 
     """ Platform is a model that represents the various video game consoles
@@ -169,7 +122,6 @@ class Platform(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     api_id = db.Column(db.Integer)
-
     created_at = db.Column(db.DateTime)
     name = db.Column(db.String(80))
     summary = db.Column(db.Text)
@@ -181,7 +133,8 @@ class Platform(db.Model):
     studio = db.relationship('Studio', backref='platform', lazy="dynamic")
 
     # makes a platform object
-    def __init__(self, api_id=None, created_at=None, name=None, summary=None, games=None, review=None, generation=None, image=None, website=None, studio=None):
+    def __init__(self, api_id=None, created_at=None, name=None, summary=None,
+                 games=None, review=None, generation=None, image=None, website=None, studio=None):
 
         if api_id != None:
             assert api_id > -1
@@ -222,28 +175,6 @@ class Platform(db.Model):
             assert isinstance(studio, Studio)
             self.studio = studio
 
-        # assert api_id > 0
-        # assert created_at != None
-        # assert name != ''
-        # assert summary != None
-        # assert games != None
-        # assert review != None
-        # assert generation > -1 and generation < 100
-        # assert image != None
-        # assert website != None
-        # assert studio != None
-
-        # self.api_id = api_id
-        # self.created_at = created_at
-        # self.name = name
-        # self.summary = summary
-        # self.games = games
-        # self.review = review
-        # self.generation = generation
-        # self.image = image
-        # self.website = website
-        # self.studio = studio
-
 
 class Studio(db.Model):
 
@@ -256,7 +187,6 @@ class Studio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
     platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'))
-
     logo = db.Column(db.String(256))
     description = db.Column(db.Text, nullable=True)
     game = db.relationship('Game', backref='studio', lazy="dynamic")
@@ -293,21 +223,7 @@ class Studio(db.Model):
         if website != None:
             assert website != ""
             self.website = website
-        # assert name != ''
-        # assert platform_id >= 0
-        # assert logo != None
-        # assert description != None
-        # assert game != None
-        # assert created_at != None
-        # assert website != None
-        # self.name = name
-        # self.platform_id = platform_id
-        # self.logo = logo
-        # self.description = description
-        # self.game = game
-        # self.created_at = created_at
-        # self.website = website
-
+       
 
 class Reviews(db.Model):
 
@@ -321,12 +237,10 @@ class Reviews(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256))
-
     platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"))
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
     created_at = db.Column(db.DateTime)
     views = db.Column(db.Integer)
-
     video = db.Column(db.String(256))
     introduction = db.Column(db.Text, nullable=True)
     content = db.Column(db.Text)
@@ -386,27 +300,3 @@ class Reviews(db.Model):
         if url != None:
             assert url != "None"
             self. url = url
-        # assert title != ''
-        # assert platform_id != None
-        # assert game_id != None
-        # assert created_at != None
-        # assert views >= 0
-        # assert video != None
-        # assert introduction != None
-        # assert content != None
-        # assert conclusion != None
-        # assert positive != None
-        # assert negative != None
-        # assert url != None
-        # self.title = title
-        # self.platform_id = platform_id
-        # self.game_id = game_id
-        # self.created_at = created_at
-        # self.views = views
-        # self.video = video
-        # self.introduction = introduction
-        # self.content = content
-        # self.conclusion = conclusion
-        # self.positive = positive
-        # self.negative = negative
-        # self.url = url
