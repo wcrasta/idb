31e2b4d946626096c009b41c4f40e8399ef02e40
wcrasta <wcrasta@utexas.edu>
2017-04-14 13:19:02 -0500

    Colin's changes.

--- app/visualizationScrap.py
+++ app/visualizationScrap.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import sys
 
 def get_categories():
 	url = 'http://youtubesweg.me/api/category'
@@ -11,9 +12,40 @@ def get_categories():
 				for z in x[y]:
 					tempdict = {}
 					tempdict['title'] = z['title']
-					tempdict['channels'] = z['channels'] 
-					print(tempdict['title'])
-					print(tempdict['channels'])
+					tempdict['channels'] = z['channels']
+					listHolder += [tempdict]
+	return listHolder
 
-get_categories()
-##print(r.json())
+def get_channels():
+	url = 'http://youtubesweg.me/api/channel'
+	listHolder = list()
+	r = requests.get(url)
+	for x in r.json():
+		for y in x:
+			if y == 'channels':
+				for z in x[y]:
+					tempdict = {}
+					tempdict['id'] = z['id']
+					tempdict['title'] = z['title']
+					tempdict['videos'] = z['videos']
+					tempdict['view_count'] = z['view_count']
+					listHolder += [tempdict]
+	return listHolder
+
+def get_videos():
+	url = 'http://youtubesweg.me/api/video'
+	listHolder = list()
+	r = requests.get(url)
+	for x in r.json():
+		for y in x:
+			if y == 'videos':
+				for z in x[y]:
+					tempdict = {}
+					tempdict['id'] = z['id']
+					tempdict['title'] = z['title']
+					tempdict['video_url'] = z['video_url']
+					listHolder += [tempdict]
+	return listHolder
+
+output = [{'categories':get_categories(), 'channels':get_channels(), 'videos':get_videos()}] 
+print(output)
